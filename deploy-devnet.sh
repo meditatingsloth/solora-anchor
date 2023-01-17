8eb5c668f7b4ab76bcd39035d3d34c4a204f6b29
Austin Borden <austin.borden@gmail.com>
2023-01-17 13:22:55 +0700

    prevent building everything on deploy

--- deploy-devnet.sh
+++ deploy-devnet.sh
@@ -5,7 +5,7 @@ PROGRAM="SPPBWBa5ooYYTVZDT7ESkU8ui7uiUjok6YBNeDogbx5"
 DEPLOYER="DPLiu1NuyaFKUbf87Wes4tE85jXT18NS4MpptGbksAVR"
 
 solana config set --url https://api.devnet.solana.com
-anchor build
+anchor build -p $PROGRAM_NAME
 cp ./target/idl/${PROGRAM_NAME}.json ../solora-monorepo/packages/solora-ts/src/${PROGRAM_NAME}.json
 solana program deploy -k ./keys/${DEPLOYER}.json ./target/deploy/$PROGRAM_NAME.so
 anchor idl upgrade ${PROGRAM} -f ./target/idl/$PROGRAM_NAME.json --provider.cluster devnet --provider.wallet ./keys/${DEPLOYER}.json
--- deploy-mainnet.sh
+++ deploy-mainnet.sh
@@ -5,7 +5,7 @@ PROGRAM="SPPBWBa5ooYYTVZDT7ESkU8ui7uiUjok6YBNeDogbx5"
 DEPLOYER="DPLiu1NuyaFKUbf87Wes4tE85jXT18NS4MpptGbksAVR"
 
 solana config set --url https://west.sentries.io/2ceb6a385b424fa38ef3482f4a31e9c9
-anchor build
-cp ./target/idl/${PROGRAM_NAME}.json ../solora-ui/src/idls/${PROGRAM_NAME}.json
+anchor build -p $PROGRAM_NAME
+cp ./target/idl/${PROGRAM_NAME}.json ../solora-monorepo/packages/solora-ts/src/${PROGRAM_NAME}.json
 solana program deploy -k ./keys/${DEPLOYER}.json ./target/deploy/$PROGRAM_NAME.so
 anchor idl upgrade ${PROGRAM} -f ./target/idl/$PROGRAM_NAME.json --provider.cluster mainnet --provider.wallet ./keys/${DEPLOYER}.json
