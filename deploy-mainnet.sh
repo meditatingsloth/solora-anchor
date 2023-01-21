43abd57e2e43edeb4b03251e6654998ea6544142
Austin Borden <austin.borden@gmail.com>
2023-01-21 23:36:23 +0700

    ignore

--- deploy-mainnet.sh
+++ deploy-mainnet.sh
@@ -4,7 +4,7 @@ PROGRAM_NAME="solora_pyth_price"
 PROGRAM="SPPq79wtPSBeFvYJbSxS9Pj1JdbQARDWxwJBXyTVcRg"
 DEPLOYER="DPLiu1NuyaFKUbf87Wes4tE85jXT18NS4MpptGbksAVR"
 
-solana config set --url https://rpc.helius.xyz?api-key=3cf96e28-5ae3-4f9d-8d08-df4bb56cd769
+solana config set --url https://west.sentries.io/2ceb6a385b424fa38ef3482f4a31e9c9
 anchor build -p $PROGRAM_NAME
 cp ./target/idl/${PROGRAM_NAME}.json ../solora-monorepo/packages/solora-ts/src/${PROGRAM_NAME}.json
 solana program deploy -k ./keys/${DEPLOYER}.json ./target/deploy/$PROGRAM_NAME.so
