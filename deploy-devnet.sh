d2c2cf84eb8850b86e486f563e7497a78e03f40a
Austin Borden <austin.borden@gmail.com>
2023-01-23 07:48:34 +0700

    optional idl in deploy scripts

--- deploy-devnet.sh
+++ deploy-devnet.sh
@@ -8,4 +8,11 @@ solana config set --url https://api.devnet.solana.com
 anchor build -p $PROGRAM_NAME
 cp ./target/idl/${PROGRAM_NAME}.json ../solora-monorepo/packages/solora-ts/src/${PROGRAM_NAME}.json
 solana program deploy -k ./keys/${DEPLOYER}.json ./target/deploy/$PROGRAM_NAME.so
-anchor idl upgrade ${PROGRAM} -f ./target/idl/$PROGRAM_NAME.json --provider.cluster devnet --provider.wallet ./keys/${DEPLOYER}.json
+
+if [ "$1" = "idl-init" ]
+  anchor idl init ${PROGRAM} -f ./target/idl/$PROGRAM_NAME.json --provider.cluster devnet --provider.wallet ./keys/${DEPLOYER}.json
+fi
+
+if [ "$1" = "idl-upgrade" ]
+  anchor idl upgrade ${PROGRAM} -f ./target/idl/$PROGRAM_NAME.json --provider.cluster devnet --provider.wallet ./keys/${DEPLOYER}.json
+fi
